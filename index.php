0e2be8c661ce8d8810e79759c948c651f23d0bcf
kusuma-656 <143714718+kusuma-656@users.noreply.github.com>
2023-09-02 14:15:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,143 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-session_start();
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CRoboto" rel="stylesheet">
+  <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-include("includes/db.php");
-include("includes/header.php");
-include("functions/functions.php");
-include("includes/main.php");
+<link rel="shortcut icon" href="//cdn.shopify.com/s/files/1/2484/9148/files/SDQSDSQ_32x32.png?v=1511436147" type="image/png">
+  <title>Ecommerce PHP</title>
+  <link href="styles/bootstrap.min.css" rel="stylesheet">
+  <link href="styles/backend.css" rel="stylesheet">
+  <link href="styles/style.css" rel="stylesheet">
 
-?>
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
+</head>
+
+<body>
+
+  <header class="page-header">
+    <!-- topline -->
+    <div class="page-header__topline">
+      <div class="container clearfix">
+
+        <div class="currency">
+          <a class="currency__change" href="customer/my_account.php?my_orders">
+          Welcome :Guest          </a>
+        </div>
+
+        <div class="basket">
+          <a href="cart.php" class="btn btn--basket">
+            <i class="icon-basket"></i>
+            3 items
+          </a>
+        </div>
+        
+        
+        <ul class="login">
+
+<li class="login__item">
+<a href="customer_register.php" class="login__link">Register</a>  
+</li>
+
+
+<li class="login__item">
+<a href="checkout.php" class="login__link">Sign In</a>  
+  
+</li>
+</ul>
+      
+      </div>
+    </div>
+    <!-- bottomline -->
+    <div class="page-header__bottomline">
+      <div class="container clearfix">
+
+        <div class="logo">
+          <a class="logo__link" href="index.php">
+            <img class="logo__img" src="images/logo.png" alt="Avenue fashion logotype" width="237" height="19">
+          </a>
+        </div>
+
+        <nav class="main-nav">
+          <ul class="categories">
+
+            <li class="categories__item">
+              <a class="categories__link" href="#">
+                Mens
+               
+              </a>
+              </li>
+
+            <li class="categories__item">
+              <a class="categories__link" href="#">
+                Womens
+               
+              </a>
+            </li>
+
+            <li class="categories__item">
+              <a class="categories__link categories__link--active" href="shop.php">
+                Shop
+              </a>
+            </li>
+
+            <li class="categories__item">
+              <a class="categories__link" href="localstore.php">
+                Local Stores
+              </a>
+            </li>
+
+          <li class="categories__item">
+              <a class="categories__link" href="customer/my_account.php?my_orders">
+                My Account
+                <i class="icon-down-open-1"></i>
+              </a>
+              <div class="dropdown dropdown--lookbook">
+                <div class="clearfix">
+                  <div class="dropdown__half">
+                    <div class="dropdown__heading">Account Settings</div>
+                    <ul class="dropdown__items">
+                      <li class="dropdown__item">
+                        <a href="#" class="dropdown__link">My Wishlist</a>
+                      </li>
+                      <li class="dropdown__item">
+                        <a href="#" class="dropdown__link">My Orders</a>
+                      </li>
+                      <li class="dropdown__item">
+                        <a href="#" class="dropdown__link">View Shopping Cart</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="dropdown__half">
+                    <div class="dropdown__heading"></div>
+                    <ul class="dropdown__items">
+                      <li class="dropdown__item">
+                        <a href="#" class="dropdown__link">Edit Your Account</a>
+                      </li>
+                      <li class="dropdown__item">
+                        <a href="#" class="dropdown__link">Change Password</a>
+                      </li>
+                      <li class="dropdown__item">
+                        <a href="#" class="dropdown__link">Delete Account</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+             
+
+              </div>
+
+            </li>
+
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 
   <!-- Cover -->
   <main>
@@ -29,11 +158,412 @@ include("includes/main.php");
 
     <div class="row"><!-- row Starts -->
 
-    <?php
+    
 
-    getPro();
+<div class='col-md-4 col-sm-6 single' >
 
-    ?>
+<div class='product' >
+
+<a href='cotton-polo-shirt' >
+
+<img src='admin_area/product_images/g18bulk.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Gildan 1800 </p>
+
+</center>
+
+<hr>
+
+<h3><a href='cotton-polo-shirt' >Gildan 1800 Ultra Cotton Polo Shirt</a></h3>
+
+<p class='price' > <del> $88 </del> | $75 </p>
+
+<p class='buttons' >
+
+<a href='cotton-polo-shirt' class='btn btn-default' >View Details</a>
+
+<a href='cotton-polo-shirt' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>Sale</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<div class='col-md-4 col-sm-6 single' >
+
+<div class='product' >
+
+<a href='demo-product-showcase' >
+
+<img src='admin_area/product_images/Prod-placeholder.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Polo </p>
+
+</center>
+
+<hr>
+
+<h3><a href='demo-product-showcase' >Demo Product Title Name - Test</a></h3>
+
+<p class='price' > $120  </p>
+
+<p class='buttons' >
+
+<a href='demo-product-showcase' class='btn btn-default' >View Details</a>
+
+<a href='demo-product-showcase' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>Test</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<div class='col-md-4 col-sm-6 single' >
+
+<div class='product' >
+
+<a href='nike-sportswear-essen-col' >
+
+<img src='admin_area/product_images/nike-s.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Nike </p>
+
+</center>
+
+<hr>
+
+<h3><a href='nike-sportswear-essen-col' >Nike Sportswear Essential Collection</a></h3>
+
+<p class='price' > $90  </p>
+
+<p class='buttons' >
+
+<a href='nike-sportswear-essen-col' class='btn btn-default' >View Details</a>
+
+<a href='nike-sportswear-essen-col' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>Featured</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<div class='col-md-4 col-sm-6 single' >
+
+<div class='product' >
+
+<a href='ultraboost-21-adidas' >
+
+<img src='admin_area/product_images/Ultraboost_21.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Adidas </p>
+
+</center>
+
+<hr>
+
+<h3><a href='ultraboost-21-adidas' >Ultraboost 21 PrimeBlue Shoes</a></h3>
+
+<p class='price' > $150  </p>
+
+<p class='buttons' >
+
+<a href='ultraboost-21-adidas' class='btn btn-default' >View Details</a>
+
+<a href='ultraboost-21-adidas' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>New</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<div class='col-md-4 col-sm-6 single' >
+
+<div class='product' >
+
+<a href='product-url-9' >
+
+<img src='admin_area/product_images/product-1.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Polo </p>
+
+</center>
+
+<hr>
+
+<h3><a href='product-url-9' >Long Sleeves Polo Shirt for Men</a></h3>
+
+<p class='price' > <del> $50 </del> | $35 </p>
+
+<p class='buttons' >
+
+<a href='product-url-9' class='btn btn-default' >View Details</a>
+
+<a href='product-url-9' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>Sale</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<div class='col-md-4 col-sm-6 single' >
+
+<div class='product' >
+
+<a href='product-url-8' >
+
+<img src='admin_area/product_images/Black Blouse Versace Coat1.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Philip Plein </p>
+
+</center>
+
+<hr>
+
+<h3><a href='product-url-8' >Sleeveless Flaux Fur Hybrid Coat</a></h3>
+
+<p class='price' > $245  </p>
+
+<p class='buttons' >
+
+<a href='product-url-8' class='btn btn-default' >View Details</a>
+
+<a href='product-url-8' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>New</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<div class='col-md-4 col-sm-6 single' >
+
+<div class='product' >
+
+<a href='product-url-5' >
+
+<img src='admin_area/product_images/Next-Denim-Borg-Lined-Western-Jacket-0463-0064553-1-pdp_slider_l.jpg' class='img-responsive' >
+
+</a>
+
+<div class='text' >
+
+<center>
+
+<p class='btn btn-warning'> Lacoste </p>
+
+</center>
+
+<hr>
+
+<h3><a href='product-url-5' >Denim Borg Lined Western Jacket</a></h3>
+
+<p class='price' > <del> $259 </del> | $100 </p>
+
+<p class='buttons' >
+
+<a href='product-url-5' class='btn btn-default' >View Details</a>
+
+<a href='product-url-5' class='btn btn-danger'>
+
+<i class='fa fa-shopping-cart'></i> Add To Cart
+
+</a>
+
+
+</p>
+
+</div>
+
+
+
+<a class='label sale' href='#' style='color:black;'>
+
+<div class='thelabel'>Gift</div>
+
+<div class='label-background'> </div>
+
+</a>
+
+
+
+
+</div>
+
+</div>
+
 
     </div><!-- row Ends -->
 
@@ -159,7 +689,7 @@ include("includes/main.php");
         <div class="container clearfix">
 
           <div class="copyright">
-            &copy; <?php echo date("Y");?> Ecommerce Website-PHP&trade;
+            &copy; 2023 Ecommerce Website-PHP&trade;
           </div>
 
           <div class="developer">
